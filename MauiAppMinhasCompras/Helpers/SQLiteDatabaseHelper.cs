b39d4060409f1ca10e96474e0f9ad2d4ba4b6786
VGamer <VGamer@WIN-JKK26Q80U21>
2025-08-13 16:12:59 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppMinhasCompras/Helpers/SQLiteDatabaseHelper.cs
@@ -0,0 +1,42 @@
+﻿using SQLite;
+
+namespace MauiAppMinhasCompras.Models
+{
+    public class SQLiteDataBaseHelper
+    {
+        private readonly SQLiteAsyncConnection _connection;
+
+        public SQLiteDataBaseHelper(string dbPath)
+        {
+            _connection = new SQLiteAsyncConnection(dbPath);
+            _connection.CreateTableAsync<Produto>().Wait();
+        }
+
+        public Task<int> Insert(Produto p)
+        {
+            return _connection.InsertAsync(p);
+        }
+
+        public Task<int> Update(Produto p)
+        {
+            string sql = "UPDATE Produto SET Descricao=?, Quantidade=?, Preco=? WHERE Id=?";
+            return _connection.ExecuteAsync(sql, p.Descricao, p.Quantidade, p.Preco, p.Id);
+        }
+
+        public Task<int> Delete(int id)
+        {
+            return _connection.Table<Produto>().DeleteAsync(i => i.Id == id);
+        }
+
+        public Task<List<Produto>> GetAll()
+        {
+            return _connection.Table<Produto>().ToListAsync();
+        }
+
+        public Task<List<Produto>> Search(string q)
+        {
+            string sql = $"SELECT * FROM Produto WHERE Descricao LIKE '%{q}%'";
+            return _connection.QueryAsync<Produto>(sql);
+        }
+    }
+}
